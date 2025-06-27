21dbd549e6f87e5f14a659e1b35b5d472e33b28f
CoolColdUK <coolcold@gmail.com>
2025-06-27 18:12:41 +0100

    updated to fix build

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -1,5 +1,7 @@
 import {MetadataRoute} from 'next';
 
+export const dynamic = 'force-static';
+
 export default function sitemap(): MetadataRoute.Sitemap {
   const baseUrl = 'https://simpleoutcome.dev';
 
@@ -7,19 +9,19 @@ export default function sitemap(): MetadataRoute.Sitemap {
     {
       url: baseUrl,
       lastModified: new Date(),
-      changeFrequency: 'weekly',
+      changeFrequency: 'monthly',
       priority: 1,
     },
     {
       url: `${baseUrl}/projects`,
       lastModified: new Date(),
-      changeFrequency: 'weekly',
+      changeFrequency: 'monthly',
       priority: 0.8,
     },
     {
       url: `${baseUrl}/contact`,
       lastModified: new Date(),
-      changeFrequency: 'weekly',
+      changeFrequency: 'yearly',
       priority: 0.5,
     },
   ];
