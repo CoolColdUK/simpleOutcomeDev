f98a73bc40f763486bc0483c0b770d9b8d84a1f5
CoolColdUK <coolcold@gmail.com>
2025-06-27 16:47:50 +0100

    update url

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -7,6 +7,6 @@ export default function robots(): MetadataRoute.Robots {
       allow: '/',
       disallow: ['/private/', '/admin/'],
     },
-    sitemap: 'https://simpleoutcome.com/sitemap.xml',
+    sitemap: 'https://simpleoutcome.dev/sitemap.xml',
   };
 }
--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -1,25 +1,25 @@
 import {MetadataRoute} from 'next';
 
 export default function sitemap(): MetadataRoute.Sitemap {
-  const baseUrl = 'https://simpleoutcome.com';
+  const baseUrl = 'https://simpleoutcome.dev';
 
   return [
     {
       url: baseUrl,
       lastModified: new Date(),
-      changeFrequency: 'monthly',
+      changeFrequency: 'weekly',
       priority: 1,
     },
     {
       url: `${baseUrl}/projects`,
       lastModified: new Date(),
-      changeFrequency: 'monthly',
+      changeFrequency: 'weekly',
       priority: 0.8,
     },
     {
       url: `${baseUrl}/contact`,
       lastModified: new Date(),
-      changeFrequency: 'yearly',
+      changeFrequency: 'weekly',
       priority: 0.5,
     },
   ];
